b03a4ffc6ab3e47b24e0734c029f9cafefb52d16
Alexander <36491066+danube88@users.noreply.github.com>
2021-08-26 00:33:39 +0300

    add ui bootstrap

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 Route::group([
 	'prefix' => LaravelLocalization::setLocale(),
-	'middleware' => [
-		'localeSessionRedirect',
-		'localizationRedirect',
-		'localeViewPath'
-	]], function() {
+	'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+	], function() {
         Route::get('/', function () {
             return view('welcome');
         });
+
+		Auth::routes();
+
+		Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     });
