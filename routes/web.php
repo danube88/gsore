82d911ba54e7ac6e76688a5301072f2f2ff74fec
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-02 00:10:11 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::group([
             return view('welcome');
         });
 
+		Route::get('/index', function () {
+            return view('index');
+        });
+
 		Auth::routes();
 
 		Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
