73bdfcd87f92fdbbf070d5a75c367b3aaafec338
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-03 22:49:46 +0300

    update home page

--- resources/views/components/home/main.blade.php
+++ resources/views/components/home/main.blade.php
@@ -1,1 +1,16 @@
+    <!-- ::::::  Start Testimonial Section  ::::::  -->
+    <x-home.section.testimonial/>
+    <!-- ::::::  End Testimonial Section  ::::::  -->
 
+
+    <!-- ::::::  Start  Blog News  ::::::  -->
+    <x-home.section.blog-news/>
+    <!-- ::::::  End  Blog News   ::::::  -->
+
+    <!-- ::::::  Start Newsletter Section  ::::::  -->
+    <x-home.section.newsletter/>
+    <!-- ::::::  End newsletter Section  ::::::  -->
+
+    <!-- ::::::  Start  Company Logo Section  ::::::  -->
+    <x-section.company-logo/>
+    <!-- ::::::  End  Company Logo Section  ::::::  -->
---
+++ resources/views/components/home/section/blog-news.blade.php
@@ -0,0 +1,109 @@
+<div class="blog m-t-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                 <!-- Start Section Title -->
+                <div class="section-content section-content--border m-b-35">
+                    <h5 class="section-content__title">Our Latest News</h5>
+                    <a href="blog-list-sidebar-left.html" class="btn btn--icon-left btn--small btn--radius btn--transparent btn--border-green btn--border-green-hover-green font--regular text-capitalize">More blogs <i class="fal fa-angle-right"></i></a>
+                </div>  <!-- End Section Title -->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="default-slider default-slider--hover-bg-red">
+                    <div class="blog-feed-slider-3grid default-slider gap__col--30 ">
+                        <!-- Start Single Blog Feed -->
+                        <div class="blog-feed">
+                            <!-- Start Blog Feed Image -->
+                            <div class="blog-feed__img-box">
+                                <a href="blog-single-sidebar-left.html" class="blog-feed__img--link">
+                                    <img class="img-fluid" src="../img/blog/feed/blog-feed-home-1-img-1.jpg" alt="">
+                                </a>
+                            </div> <!-- End  Blog Feed Image -->
+                            <!-- Start  Blog Feed Content -->
+                            <div class="blog-feed__content ">
+                                <a href="blog-single-sidebar-left.html" class="blog-feed__link">Consectetur adipisicing</a>
+
+                                <div class="blog-feed__post-meta">
+                                    By
+                                    <a class="blog-feed__post-meta--link" href="blog-grid-sidebar-left.html"><span class="blog-feed__post-meta--author">Partner 2020 /</span></a>
+                                    <a class="blog-feed__post-meta--link" href="blog-grid-sidebar-left.html"><span class="blog-feed__post-meta--date">July 23, 2020</span></a>
+
+                                </div>
+                                <p class="blog-feed__excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid...</p>
+                                <a href="blog-single-sidebar-left.html" class="btn btn--small btn--radius btn--green btn--green-hover-black font--regular text-uppercase text-capitalize">Continue Reading</a>
+                            </div> <!-- End  Blog Feed Content -->
+                        </div> <!-- End Single Blog Feed -->
+                        <!-- Start Single Blog Feed -->
+                        <div class="blog-feed">
+                            <!-- Start Blog Feed Image -->
+                            <div class="blog-feed__img-box">
+                                <a href="blog-single-sidebar-left.html" class="blog-feed__img--link">
+                                    <img class="img-fluid" src="../img/blog/feed/blog-feed-home-1-img-2.jpg" alt="">
+                                </a>
+                            </div> <!-- End  Blog Feed Image -->
+                            <!-- Start  Blog Feed Content -->
+                            <div class="blog-feed__content ">
+                                <a href="blog-single-sidebar-left.html" class="blog-feed__link">Consectetur adipisicing</a>
+
+                                <div class="blog-feed__post-meta">
+                                    By
+                                    <a class="blog-feed__post-meta--link" href="blog-grid-sidebar-left.html"><span class="blog-feed__post-meta--author">Partner 2020 /</span></a>
+                                    <a class="blog-feed__post-meta--link" href="blog-grid-sidebar-left.html"><span class="blog-feed__post-meta--date">July 23, 2020</span></a>
+
+                                </div>
+                                <p class="blog-feed__excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid...</p>
+                                <a href="blog-single-sidebar-left.html" class="btn btn--small btn--radius btn--green btn--green-hover-black font--regular text-uppercase text-capitalize">Continue Reading</a>
+                            </div> <!-- End  Blog Feed Content -->
+                        </div> <!-- End Single Blog Feed -->
+                        <!-- Start Single Blog Feed -->
+                        <div class="blog-feed">
+                            <!-- Start Blog Feed Image -->
+                            <div class="blog-feed__img-box">
+                                <a href="blog-single-sidebar-left.html" class="blog-feed__img--link">
+                                    <img class="img-fluid" src="../img/blog/feed/blog-feed-home-1-img-3.jpg" alt="">
+                                </a>
+                            </div> <!-- End  Blog Feed Image -->
+                            <!-- Start  Blog Feed Content -->
+                            <div class="blog-feed__content ">
+                                <a href="blog-single-sidebar-left.html" class="blog-feed__link">Consectetur adipisicing</a>
+
+                                <div class="blog-feed__post-meta">
+                                    By
+                                    <a class="blog-feed__post-meta--link" href="blog-grid-sidebar-left.html"><span class="blog-feed__post-meta--author">Partner 2020 /</span></a>
+                                    <a class="blog-feed__post-meta--link" href="blog-grid-sidebar-left.html"><span class="blog-feed__post-meta--date">July 23, 2020</span></a>
+
+                                </div>
+                                <p class="blog-feed__excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid...</p>
+                                <a href="blog-single-sidebar-left.html" class="btn btn--small btn--radius btn--green btn--green-hover-black font--regular text-uppercase text-capitalize">Continue Reading</a>
+                            </div> <!-- End  Blog Feed Content -->
+                        </div> <!-- End Single Blog Feed -->
+                        <!-- Start Single Blog Feed -->
+                        <div class="blog-feed">
+                            <!-- Start Blog Feed Image -->
+                            <div class="blog-feed__img-box">
+                                <a href="blog-single-sidebar-left.html" class="blog-feed__img--link">
+                                    <img class="img-fluid" src="../img/blog/feed/blog-feed-home-1-img-4.jpg" alt="">
+                                </a>
+                            </div> <!-- End  Blog Feed Image -->
+                            <!-- Start  Blog Feed Content -->
+                            <div class="blog-feed__content ">
+                                <a href="blog-single-sidebar-left.html" class="blog-feed__link">Consectetur adipisicing</a>
+
+                                <div class="blog-feed__post-meta">
+                                    By
+                                    <a class="blog-feed__post-meta--link" href="blog-grid-sidebar-left.html"><span class="blog-feed__post-meta--author">Partner 2020 /</span></a>
+                                    <a class="blog-feed__post-meta--link" href="blog-grid-sidebar-left.html"><span class="blog-feed__post-meta--date">July 23, 2020</span></a>
+
+                                </div>
+                                <p class="blog-feed__excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid...</p>
+                                <a href="blog-single-sidebar-left.html" class="btn btn--small btn--radius btn--green btn--green-hover-black font--regular text-uppercase text-capitalize">Continue Reading</a>
+                            </div> <!-- End  Blog Feed Content -->
+                        </div> <!-- End Single Blog Feed -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
