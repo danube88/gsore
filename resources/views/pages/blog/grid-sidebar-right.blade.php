81a3c60071ebf9ec1dcb002c694e1fc72fbec32f
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-07 22:37:13 +0300

    create page grid-sidebar-right

---
+++ resources/views/pages/blog/grid-sidebar-right.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.siteApp')
+
+@section('header')
+    <x-header.header/>
+    <x-header.mobile-header/>
+    <x-header.mobile-offcanvas-menu/>
+    <x-header.popup-add-cart/>
+    <x-header.offcanvas-overlay/>
+@endsection
+
+@section('breadcrumb')
+    <li class="page-breadcrumb__nav"><a href="{{ route('index') }}">Home</a></li>
+    <li class="page-breadcrumb__nav active">Blog Grid Sidebar Right</li>
+@endsection
+
+@section('main')
+    <x-blog.grid-sidebar.right-main/>
+@endsection
+
+@section('footer')
+    <x-footer.footer/>
+@endsection
