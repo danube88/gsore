004d607607adc4d90dbc11188919dee378ef39bf
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-08 00:15:45 +0300

    Creat page list-sidebar left and right

---
+++ resources/views/components/blog/list-sidebar/left-main.blade.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row flex-column-reverse flex-lg-row">
+        <!-- Start Leftside - Sidebar Widgets -->
+        <div class="col-lg-3">
+            <div class="sidebar">
+                <!-- Start Single Sidebar Widget - Custom Menu -->
+                <x-blog.section.menu/>
+                <!-- End Single Sidebar Widget - Custom Menu -->
+
+                <!-- Start Single Sidebar Widget - Recent Post -->
+                <x-blog.section.post/>
+                <!-- End Single Sidebar Widget - Recent Post  -->
+
+                <!-- ::::::  Start Single Sidebar Widget - Tag   ::::::  -->
+                <x-blog.section.tag/>
+                <!-- ::::::  End Single Sidebar Widget - Recent Post   ::::::  -->
+            </div>
+        </div>  <!-- End Left Sidebar  Widgets-->
+
+        <!-- Start Rightside - Content -->
+        <div class="col-lg-9">
+            <div class="blog">
+                <x-blog.list-sidebar.section.blog/>
+            </div>
+
+            <div class="page-pagination">
+                <ul class="page-pagination__list">
+                    <li class="page-pagination__item"><a class="page-pagination__link"  href="#">Prev</a>
+                    <li class="page-pagination__item"><a class="page-pagination__link active"  href="#">1</a></li>
+                    <li class="page-pagination__item"><a class="page-pagination__link"  href="#">2</a></li>
+                    <li class="page-pagination__item"><a class="page-pagination__link"  href="#">Next</a>
+                    </li>
+                  </ul>
+            </div>
+        </div>
+    </div>  <!-- End Rightside - Content -->
+</div>
---
+++ resources/views/components/blog/list-sidebar/section/blog.blade.php
@@ -0,0 +1,113 @@
+<div class="row">
+    <!-- Start Single Blog List -->
+    <div class="col-12">
+        <div class="blog__type-list">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="blog__img"><a href=""><img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-1.jpg') }}" alt=""></a></div>
+                </div>
+                <div class="col-md-7">
+                    <div class="blog__content">
+                        <a class="link--gray" href=""><h3 class="blog__title">Consectetur Adipisicing</h3></a>
+                        <div class="blog__archive m-t-20">
+                            <a href="#" class="link--gray link--icon-left m-r-30"><i class="far fa-calendar"></i> Oct 29, 2018</a>
+                            <a href="#" class="link--gray link--icon-left"><i class="far fa-user"></i> Jhon Doe</a>
+                        </div>
+                        <div class="para m-tb-20">
+                            <p class="para__text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus expedita quo voluptate, esse ratione facilis quibusdam vel ad, quas ut reiciendis neque voluptates adipisci laborum, dolor saepe natus.
+                            </p>
+                        </div>
+                        <a class="btn btn--radius btn--small btn--black btn--black-hover-green" href="blog-single-sidebar-left.html">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Single Blog List -->
+    <!-- Start Single Blog List -->
+    <div class="col-12">
+        <div class="blog__type-list">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="inner-slider inner-slider-grid-1 pos-relative">
+                        <div class="blog__img"><img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-2.jpg') }}" alt=""></div>
+                        <div class="blog__img"><img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-1.jpg') }}" alt=""></div>
+                        <div class="blog__img"><img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-3.jpg') }}" alt=""></div>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="blog__content">
+                        <a href=""><h3 class="blog__title">Without Very Darkness</h3></a>
+                        <div class="blog__archive m-t-20">
+                            <a href="#" class="link--gray link--icon-left m-r-30"><i class="far fa-calendar"></i> Jul 9, 2018</a>
+                            <a href="#" class="link--gray link--icon-left"><i class="far fa-user"></i> Jhon Doe</a>
+                        </div>
+                        <div class="para m-tb-20">
+                            <p class="para__text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus expedita quo voluptate, esse ratione facilis quibusdam vel ad, quas ut reiciendis neque voluptates adipisci laborum, dolor saepe natus.
+                            </p>
+                        </div>
+                            <a class="btn btn--radius btn--small btn--black btn--black-hover-green" href="blog-single-sidebar-left.html">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Single Blog List -->
+    <!-- Start Single Blog List -->
+    <div class="col-12">
+        <div class="blog__type-list">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="blog__img">
+                        <div class="blog__video pos-relative">
+                            <img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-4.jpg') }}" alt="">
+                            <a class="popup-icon vinobox-popup pos-absolute" data-autoplay="true" data-vbtype="video" href="https://youtu.be/QQHLGlB4Dpk?list=PLk25BQFrj7wHgCkzPuejFXJTiFIam-MVq"><i class="fal fa-play"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="blog__content">
+                                <a href=""><h3 class="blog__title">How to Diet Properly</h3></a>
+                        <div class="blog__archive m-t-20">
+                            <a href="#" class="link--gray link--icon-left m-r-30"><i class="far fa-calendar"></i> Mar 3, 2018</a>
+                            <a href="#" class="link--gray link--icon-left"><i class="far fa-user"></i> Jhon Doe</a>
+                        </div>
+                        <div class="para m-tb-20">
+                            <p class="para__text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus expedita quo voluptate, esse ratione facilis quibusdam vel ad, quas ut reiciendis neque voluptates adipisci laborum, dolor saepe natus.
+                            </p>
+                        </div>
+                        <a class="btn btn--radius btn--small btn--black btn--black-hover-green" href="blog-single-sidebar-left.html">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Single Blog List -->
+    <!-- Start Single Blog List -->
+    <div class="col-12">
+        <div class="blog__type-list">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="blog__img">
+                        <iframe class="embed-responsive-item" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/182537870&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="blog__content">
+                        <a class="link--gray" href=""><h3 class="blog__title">Remaining Essential Foods</h3></a>
+                        <div class="blog__archive m-t-20">
+                            <a href="#" class="link--gray link--icon-left m-r-30"><i class="far fa-calendar"></i> Feb 11, 2018</a>
+                            <a href="#" class="link--gray link--icon-left"><i class="far fa-user"></i> Jhon Doe</a>
+                        </div>
+                        <div class="para m-tb-20">
+                            <p class="para__text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus expedita quo voluptate, esse ratione facilis quibusdam vel ad, quas ut reiciendis neque voluptates adipisci laborum, dolor saepe natus.
+                            </p>
+                        </div>
+                        <a class="btn btn--radius btn--small btn--black btn--black-hover-green" href="blog-single-sidebar-left.html">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Single Blog List -->
+</div>
