00a399e2a860c1e76a8db17dfad03830cf1a4ab8
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-08 00:15:07 +0300

    creat grid-sidebar section blog

---
+++ app/View/Components/Blog/GridSidebar/Section/Blog.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components\Blog\GridSidebar\Section;
+
+use Illuminate\View\Component;
+
+class Blog extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.blog.grid-sidebar.section.blog');
+    }
+}
