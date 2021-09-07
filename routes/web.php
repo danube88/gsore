dd6148c0725eb74f47aca8890d2e1f0caeee60fe
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-08 00:03:04 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,13 +34,39 @@ Route::group([
 
 		Route::group(['prefix' => 'blog'], function()
 		{
-			Route::get('/grid-sidebar-left', function () {
-	            return view('pages.blog.grid-sidebar-left');
-	        })->name('blogGridSidebarLeft');
+			Route::group(['prefix' => 'grid-sidebar'], function()
+			{
+				Route::get('/left', function () {
+		            return view('pages.blog.grid-sidebar-left');
+		        })->name('blogGridSidebarLeft');
 
-			Route::get('/grid-sidebar-right', function () {
-	            return view('pages.blog.grid-sidebar-right');
-	        })->name('blogGridSidebarRight');
+				Route::get('/right', function () {
+		            return view('pages.blog.grid-sidebar-right');
+		        })->name('blogGridSidebarRight');
+			});
+
+			Route::group(['prefix' => 'list-sidebar'], function()
+			{
+				Route::get('/left', function () {
+		            return view('pages.blog.list-sidebar-left');
+		        })->name('blogListSidebarLeft');
+
+				Route::get('/right', function () {
+		            return view('pages.blog.list-sidebar-right');
+		        })->name('blogListSidebarRight');
+			});
+
+			Route::group(['prefix' => 'single-sidebar'], function()
+			{
+				Route::get('/left', function () {
+		            return view('pages.blog.single-sidebar-left');
+		        })->name('blogSingleSidebarLeft');
+
+				Route::get('/right', function () {
+		            return view('pages.blog.single-sidebar-right');
+		        })->name('blogSingleSidebarRight');
+			});
+
 		});
 
 		Route::group(['prefix' => 'shop'], function()
