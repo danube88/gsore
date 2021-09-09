6a2e07e2f343010f71134307b3014c9ae71376a1
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-10 00:56:59 +0300

    Create page empty cart

---
+++ resources/views/components/shop/pages/empty-cart/main.blade.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="section-content">
+                <h5 class="section-content__title">Your cart item</h5>
+            </div>
+            <div class="empty-cart m-t-40 text-center">
+                <div class="empty-cart-icon title--large"><i class="fal fa-shopping-cart"></i></div>
+                <h3 class="title title--normal title--thin m-tb-30">There are no more items in your cart</h3>
+                <a href="#" class="btn btn--box btn--large btn--radius btn--green btn--green-hover-black btn--uppercase font--semi-bold m-t-20">CONTINUE SHOPPING</a>
+            </div>
+        </div>
+    </div>
+</div>
