b2a4f69eddff44a9ce9aff5508f35b3dcae69ab5
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-10 00:56:39 +0300

    Create page compare

---
+++ resources/views/components/shop/pages/compare/main.blade.php
@@ -0,0 +1,123 @@
+<div class="container">
+    <div class="row">
+       <div class="col-12">
+        <div class="compare-area mtb-50px">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <!-- Compare Table -->
+                        <div class="compare-table table-responsive">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <td class="first-column">Product</td>
+                                        <td class="product-image-title">
+                                            <a href="#" class="img-responsive m-b-15"><img  src="{{ config('app.url') }}/img/product/size-normal/product-home-1-img-1.jpg" alt="Compare Product"></a>
+                                            <a href="#" class="category">Vagitables</a>
+                                            <a href="#" class="title">Green Pee</a>
+                                        </td>
+                                        <td class="product-image-title">
+                                            <a href="#" class="img-responsive m-b-15"><img src="{{ config('app.url') }}/img/product/size-normal/product-home-1-img-2.jpg" alt="Compare Product"></a>
+                                            <a href="#" class="category">Fishes</a>
+                                            <a href="#" class="title">Red Curp</a>
+                                        </td>
+                                        <td class="product-image-title">
+                                            <a href="#" class="img-responsive m-b-15"><img  src="{{ config('app.url') }}/img/product/size-normal/product-home-1-img-3.jpg" alt="Compare Product"></a>
+                                            <a href="#" class="category">Fruits</a>
+                                            <a href="#" class="title">Red Pomegranate</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="first-column">Description</td>
+                                        <td class="pro-desc">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas vel a qui repellendus reiciendis! Laudantium, veritatis sunt! Provident dolorem</p>
+                                        </td>
+                                        <td class="pro-desc">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas vel a qui repellendus reiciendis! Laudantium, veritatis sunt! Provident dolorem</p>
+                                        </td>
+                                        <td class="pro-desc">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas vel a qui repellendus reiciendis! Laudantium, veritatis sunt! Provident dolorem</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="first-column">Price</td>
+                                        <td class="pro-price">$29</td>
+                                        <td class="pro-price">$27</td>
+                                        <td class="pro-price">$39</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="first-column">Color</td>
+                                        <td class="pro-color">Green</td>
+                                        <td class="pro-color">Marun</td>
+                                        <td class="pro-color">Red</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="first-column">Stock</td>
+                                        <td class="pro-stock">In Stock</td>
+                                        <td class="pro-stock">In Stock</td>
+                                        <td class="pro-stock">In Stock</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="first-column">Add to cart</td>
+                                        <td class="pro-addtocart">
+                                            <a href="#modalAddCart" data-toggle="modal" class="btn btn--box btn--small btn--radius btn--green btn--green-hover-black btn--uppercase font--semi-bold">ADD TO CART</a>
+                                        </td>
+                                        <td class="pro-addtocart">
+                                            <a href="#modalAddCart" data-toggle="modal" class="btn btn--box btn--small btn--radius btn--green btn--green-hover-black btn--uppercase font--semi-bold">ADD TO CART</a>
+                                        </td>
+                                        <td class="pro-addtocart">
+                                            <a href="#modalAddCart" data-toggle="modal" class="btn btn--box btn--small btn--radius btn--green btn--green-hover-black btn--uppercase font--semi-bold">ADD TO CART</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="first-column">Delete</td>
+                                        <td class="pro-remove">
+                                            <button><i class="fas fa-trash-alt"></i></button>
+                                        </td>
+                                        <td class="pro-remove">
+                                            <button><i class="fas fa-trash-alt"></i></button>
+                                        </td>
+                                        <td class="pro-remove">
+                                            <button><i class="fas fa-trash-alt"></i></button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="first-column">Rating</td>
+                                        <td class="pro-ratting">
+                                            <ul class="product__review justify-content-center">
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--blank"><i class="icon-star"></i></li>
+                                            </ul>
+                                        </td>
+                                        <td class="pro-ratting">
+                                            <ul class="product__review justify-content-center">
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--blank"><i class="icon-star"></i></li>
+                                            </ul>
+                                        </td>
+                                        <td class="pro-ratting">
+                                            <ul class="product__review justify-content-center">
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--fill"><i class="icon-star"></i></li>
+                                                <li class="product__review--blank"><i class="icon-star"></i></li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div>
+    </div>
+</div>
