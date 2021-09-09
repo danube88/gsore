49c9fd698481f9e27bac2fab0bb36c86f85739d8
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-10 00:55:11 +0300

    Update blog.blade.php

--- resources/views/components/blog/list-sidebar/section/blog.blade.php
+++ resources/views/components/blog/list-sidebar/section/blog.blade.php
@@ -4,7 +4,7 @@
         <div class="blog__type-list">
             <div class="row">
                 <div class="col-md-5">
-                    <div class="blog__img"><a href=""><img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-1.jpg') }}" alt=""></a></div>
+                    <div class="blog__img"><a href=""><img src="{{ config('app.url') }}/img/blog/feed/blog-feed-home-1-img-1.jpg" alt=""></a></div>
                 </div>
                 <div class="col-md-7">
                     <div class="blog__content">
@@ -30,9 +30,9 @@
             <div class="row">
                 <div class="col-md-5">
                     <div class="inner-slider inner-slider-grid-1 pos-relative">
-                        <div class="blog__img"><img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-2.jpg') }}" alt=""></div>
-                        <div class="blog__img"><img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-1.jpg') }}" alt=""></div>
-                        <div class="blog__img"><img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-3.jpg') }}" alt=""></div>
+                        <div class="blog__img"><img src="{{ config('app.url') }}/img/blog/feed/blog-feed-home-1-img-2.jpg" alt=""></div>
+                        <div class="blog__img"><img src="{{ config('app.url') }}/img/blog/feed/blog-feed-home-1-img-1.jpg" alt=""></div>
+                        <div class="blog__img"><img src="{{ config('app.url') }}/img/blog/feed/blog-feed-home-1-img-3.jpg" alt=""></div>
                     </div>
                 </div>
                 <div class="col-md-7">
@@ -60,7 +60,7 @@
                 <div class="col-md-5">
                     <div class="blog__img">
                         <div class="blog__video pos-relative">
-                            <img src="{{ URL::asset('img/blog/feed/blog-feed-home-1-img-4.jpg') }}" alt="">
+                            <img src="{{ config('app.url') }}/img/blog/feed/blog-feed-home-1-img-4.jpg" alt="">
                             <a class="popup-icon vinobox-popup pos-absolute" data-autoplay="true" data-vbtype="video" href="https://youtu.be/QQHLGlB4Dpk?list=PLk25BQFrj7wHgCkzPuejFXJTiFIam-MVq"><i class="fal fa-play"></i></a>
                         </div>
                     </div>
