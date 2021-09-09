1d210cddb7d14695efb5eb6ce25432c0608e5c55
Alexander <36491066+danube88@users.noreply.github.com>
2021-09-10 00:54:45 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -549,31 +549,6 @@
       });
     });
 
-
-    /**************************** 
-    * Google Map - Location
-    *****************************/
-
-   function init() {
-    var mapOptions = {
-        zoom: 11,
-        scrollwheel: false,
-        center: new google.maps.LatLng(40.709896, -73.995481),
-    };
-    var mapElement = document.getElementById('map');
-    var map = new google.maps.Map(mapElement, mapOptions);
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.709896, -73.995481),
-        map: map,
-        icon: '../img/icon/map-point.png',
-        animation:google.maps.Animation.BOUNCE,
-        title: 'Snazzy!'
-    });
-    }
-    google.maps.event.addDomListener(window, 'load', init);
-
-
-
     /*----------------------------------
         Scroll To Top Active
     -----------------------------------*/
